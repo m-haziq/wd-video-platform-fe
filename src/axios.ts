6379bab4194b0205f39e7dfad09fc15fbbb7dbc6
m-haziq <m_haziq@outlook.com>
2023-10-24 01:33:14 +0500

    testing environment variables

--- src/axios.ts
+++ src/axios.ts
@@ -1,11 +1,15 @@
-import axios, { AxiosInstance, AxiosResponse, AxiosError } from 'axios';
+import axios, { AxiosInstance, AxiosResponse, AxiosError } from "axios";
 
 const baseURL: string | undefined = process.env.REACT_APP_API_URL;
+console.log("env", process.env);
 const axiosInstance: AxiosInstance = axios.create({ baseURL });
 
 axiosInstance.interceptors.response.use(
   (response: AxiosResponse) => response,
-  (error: AxiosError) => Promise.reject((error.response && error.response.data) || 'Something went wrong!')
+  (error: AxiosError) =>
+    Promise.reject(
+      (error.response && error.response.data) || "Something went wrong!"
+    )
 );
 
 export default axiosInstance;
